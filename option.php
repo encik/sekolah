09021891044579539be44756e9169e992c1c8b1d
Encik Nurul Hafidah <Encik Nurul Hafidah>
2017-04-07 11:36:45 +0700

    sekolah

---
+++ option.php
@@ -0,0 +1,8 @@
+<?php
+    echo "<a href='dbdapodik'><br><br/><button> DATA SEKOLAH </button><br><br/></a><br>";
+	echo "<a href='sd.php'><button> Sekolah SD </button></a><br>";
+	echo "<a href='smp.php'><button> Sekolah SMP </button></a><br>";
+	echo "<a href='sma.php'><button> Sekolah SMA </button></a><br>";
+	echo "<a href='negeri.php'><button> Sekolah Negeri </button></a><br>";
+	echo "<a href='swasta.php'><button> Sekolah Swasta </button></a><br>";
+ ?>
